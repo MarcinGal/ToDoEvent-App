25d6dd37980a1eabd3b651a12ea6316fbe4b91c5
marcingal <marcin921125@gmail.com>
2018-11-15 22:00:30 +0100

    first try made

--- toDoScript.js
+++ toDoScript.js
@@ -1,7 +1,25 @@
-console.log('lubie 2placki')
+console.log('lubie placki')
 
-document.querySelector('event-add').addEventListener('click', () => {document.querySelector('.event-list').innerHTML = `<li>test dodany</li>`
+// document.querySelector('.event-add').addEventListener('click', () => {
+//     document.createElement
+//     document.querySelector('.event-list').innerHTML = `<li>test dodany</li>`
+//     console.log('lubie 2placki')
+// })
 
+// const myArray = []
+
+
+var Event = function () {
+    this.eventContent = document.querySelector('.event-input').value;
+    document.querySelector('.event-list').innerHTML = `<li>${this.eventContent}</li>`
+}
+
+document.querySelector('.event-add').addEventListener('click', () => {
+new Event;
 })
 
-console.log('lubie 2placki')
+// document.querySelector('.event-add').addEventListener('click', () => {
+//     document.createElement()
+//     document.querySelector('.event-list').innerHTML = `<li>test dodany</li>`
+//     console.log('kreatorem go')
+// })
