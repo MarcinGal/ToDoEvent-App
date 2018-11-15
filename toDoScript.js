229817dcd47296aa6b16dde6f96f38cd6a58e618
marcingal <marcin921125@gmail.com>
2018-11-15 20:50:10 +0100

    first commit

---
+++ toDoScript.js
@@ -0,0 +1,7 @@
+console.log('lubie 2placki')
+
+document.querySelector('event-add').addEventListener('click', () => {document.querySelector('.event-list').innerHTML = `<li>test dodany</li>`
+
+})
+
+console.log('lubie 2placki')
