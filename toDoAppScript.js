7b9efaeb639af37af8051139c498c7fedd25a773
marcingal <marcin921125@gmail.com>
2018-11-19 21:05:08 +0100

    working od delete buttons

--- toDoAppScript.js
+++ toDoAppScript.js
@@ -1,9 +1,11 @@
 
 const global = function () {
     class Event {
-        constructor(place) {
+        constructor(place, arr) {
             this.place = place || document.body
             this.init()
+            this.deleteButton = deleteButton
+            
         }
 
         init() {
@@ -11,28 +13,37 @@ const global = function () {
         }
 
         pageElements() {
+           
             const eventText = document.createElement('input')
             const addEventButton = document.createElement('button')
             const ul = document.createElement('ul')
-            const arr = [] || arr
-
-
+            const arr = []
+            let deleteButtonClass = 0
             addEventButton.innerText = 'Add Event'
 
-
             addEventButton.addEventListener('click', function () {
+                
                 const event = document.createElement('li')
                 const deleteButton = document.createElement('button')
-                event.innerText = eventText.value
-                arr.push(event.innerText)
+                deleteButton.classList.add(deleteButtonClass)
+                this.arr = arr.push(eventText.value)
+                event.innerText = arr[arr.length-1]
+                console.log('test')
+                console.log(arr)
+                deleteButtonClass++
+                
                 
                 event.style.width = 0;
                 deleteButton.style.marginBottom = '20px'
                 deleteButton.innerText = 'Delete this Event'
+                deleteButton.addEventListener('click', function () {
+                    console.log(deleteButtonClass)
+                })
+
+
                
                 ul.appendChild(event)
                 ul.appendChild(deleteButton)
-                console.log('cholera')
             })
 
             this.place.appendChild(eventText)
