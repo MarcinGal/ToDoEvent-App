2b7984eefc12962ab68dc3940006b650ba0c1d93
marcingal <marcin921125@gmail.com>
2018-11-20 23:37:42 +0100

    working add / delete events

--- toDoAppScript.js
+++ toDoAppScript.js
@@ -1,85 +1,74 @@
 
-const global = function () {
+(function () {
     class Event {
         constructor(place) {
             this.place = place || document.body
-            this.init()
             this.arr = []
-
+            this.init()
         }
 
         init() {
             this.render()
         }
 
-        pageElements() {
+        addEvent(text) {
+            this.arr.push(new SingleEvent(text))
+            this.render()
+        }
 
+        pageElements() {
             const eventText = document.createElement('input')
             const addEventButton = document.createElement('button')
-            const ul = document.createElement('ul')
             addEventButton.innerText = 'Add Event'
+            addEventButton.style.marginLeft = '10px'
 
-            addEventButton.addEventListener('click', function () {
-
-                this.arr.push(eventText.value)
-
-
-                event.style.width = 0;
-                deleteButton.style.marginBottom = '20px'
-                deleteButton.innerText = 'Delete this Event'
-                deleteButton.addEventListener('click', (e) => this.deleteClickHandler(e, index)
-                )
-
-                ul.appendChild(event)
-                ul.appendChild(deleteButton)
+            addEventButton.addEventListener('click', () => {
+                this.addEvent(eventText.value)
+                console.log(this.arr)
             })
 
             this.place.appendChild(eventText)
             this.place.appendChild(addEventButton)
-            this.place.appendChild(ul)
+
         }
 
-
-
         render() {
-            this.location.innerHTML = ''
+            this.place.innerHTML = ''
+            this.pageElements()
+           
+
             const ul = document.createElement('ul')
 
-            this.pageElements()
-
-            this.arr.forEach((task, index) => {
+            this.arr.forEach((event, index) => {
                 const li = document.createElement('li')
                 const deleteButton = document.createElement('button')
 
-                li.innerText = task.text
-                deleteButton.innerText = 'delete this event'
+                li.innerText = event.text
+                deleteButton.style.margin = '0 0 20px 10px'
+                deleteButton.innerText = 'Delete this Event'
 
                 deleteButton.addEventListener('click', (e) => this.deleteClickHandler(e, index))
 
-                this.taskIsCompletedStyle(task, li)
-
-                li.appendChild(button)
+                li.appendChild(deleteButton)
                 ul.appendChild(li)
             })
-
-            this.location.appendChild(ul)
+            this.place.appendChild(ul)
         }
 
-        deleteClickHandler(e, index) {
-            e.stopPropagation()
-            this.tasks = this.tasks.slice(0, index).concat(this.tasks.slice(index + 1))
+        deleteClickHandler(element, index) {
+            this.arr = this.arr.slice(0, index).concat(this.arr.slice(index + 1))
             this.render()
         }
 
-        myList() {
-        }
-
-        render() {
-            this.pageElements()
-        }
-
     }
+
+    class SingleEvent {
+        constructor(text) {
+            this.text = text
+        }
+    }
+
     window.Event = Event
-    const event1 = new Event(document.body)
-}
-global()
+})()
+
+const event1 = new Event(document.body)
