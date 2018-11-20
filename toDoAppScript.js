5febdc9d434985d18ed9607a0399797637683c67
marcingal <marcin921125@gmail.com>
2018-11-21 00:14:27 +0100

    working on adding touching interaction on single event click

--- toDoAppScript.js
+++ toDoAppScript.js
@@ -35,15 +35,16 @@
         render() {
             this.place.innerHTML = ''
             this.pageElements()
-           
+
 
             const ul = document.createElement('ul')
 
             this.arr.forEach((event, index) => {
                 const li = document.createElement('li')
                 const deleteButton = document.createElement('button')
-
+                this.isTouched = false
                 li.innerText = event.text
+                li.addEventListener('click', this.eventClick(event))
                 deleteButton.style.margin = '0 0 20px 10px'
                 deleteButton.innerText = 'Delete this Event'
 
@@ -60,6 +61,19 @@
             this.render()
         }
 
+        eventClick(element) {
+            this.element = element
+            if (this.element.isTouched === false){
+                this.element.isTouched = true
+                console.log('1')
+                // this.element.style.textDecoration = 'line-through'
+            }else {
+                this.element.isTouched = false
+                console.log('2')
+                // this.element.style.textDecoration = 'none'
+            }
+            
+        }
     }
 
     class SingleEvent {
